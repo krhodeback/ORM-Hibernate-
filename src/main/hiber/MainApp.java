8e15db39f7e010d85a61dea9460a2a9fb846e6c3
Yefim Savin <savin.efim2@gmail.com>
2021-07-26 19:35:17 +0500

    Initial commit

--- src/main/hiber/MainApp.java
+++ src/main/hiber/MainApp.java
@@ -16,16 +16,16 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
         User user = new User("User1", "Lastname1", "user1@mail.ru");
-        user.setCar(new Car("model1", 154789l));
+        user.setCar(new Car("model1", 154789));
         userService.add(user);
         User user2 = new User("User2", "Lastname2", "user2@mail.ru");
-        user2.setCar(new Car("model2", 784579l));
+        user2.setCar(new Car("model2", 784579));
         userService.add(user2);
         User user3 = new User("User3", "Lastname3", "user3@mail.ru");
-        user3.setCar(new Car("model3", 784968l));
+        user3.setCar(new Car("model3", 784968));
         userService.add(user3);
         User user4 = new User("User4", "Lastname4", "user4@mail.ru");
-        user4.setCar(new Car("model4", 635779l));
+        user4.setCar(new Car("model4", 635779));
         userService.add(user4);
 
         List<User> users = userService.listUsers();
@@ -39,7 +39,7 @@ public class MainApp {
             System.out.println();
         }
 
-        System.out.println("Find user by car : " + userService.getUserByCar(new Car("model4", 635779l)));
+        System.out.println("Find user by car : " + userService.getUserByCar(new Car("model4", 635779)));
 
         context.close();
     }
